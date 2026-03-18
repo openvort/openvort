adde95c0d993d01b6c93f185640b9dde54b3f353
yq <359199843@qq.com>
2026-03-18 10:21:47 +0800

    refactor: 优化工具栏显示逻辑，新增条件以隐藏工具栏

--- web/src/views/vortflow/components/VortFlowToolbar.vue
+++ web/src/views/vortflow/components/VortFlowToolbar.vue
@@ -7,11 +7,13 @@ import ProjectSelector from "./ProjectSelector.vue";
 const route = useRoute();
 const store = useVortFlowStore();
 
-const isBoard = computed(() => route.name === "vortflow-board");
+const shouldHideToolbar = computed(() =>
+    route.name === "vortflow-board" || route.name === "vortflow-settings"
+);
 </script>
 
 <template>
-    <div v-if="!isBoard" class="bg-white rounded-xl px-4 py-3 flex items-center gap-4">
+    <div v-if="!shouldHideToolbar" class="bg-white rounded-xl px-4 py-3 flex items-center gap-4">
         <ProjectSelector
             :projects="store.projects"
             :selected-id="store.selectedProjectId"
